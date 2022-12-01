2a7d3dbcff91350fc6f3374908655237f2125f24
Your Name Main <you@example.com>
2022-12-01 11:38:54 -0800

    readme file update

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,17 +1,17 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
   return (
     <Navbar bg="black"  expand="lg">
-      <Container >
-        <Navbar.Brand className='text-light' href="#home">React-Bootstrap</Navbar.Brand>
+      <Container className='nav-container'>
+        <NavLink to='/' className='text-light movie' >MovieFair</NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link className='text-light' href="#home">Home</Nav.Link>
-            <Nav.Link className='text-light' href="#link">Link</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
