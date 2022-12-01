c3c14bc89715a580f0424a0af322256be471a98a
Your Name Main <you@example.com>
2022-12-01 07:48:46 -0800

    header added

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const Header = () => {
-    return (
-        <div>
-            <h2>Header</h2>
-        </div>
-    );
-};
+function Header() {
+  return (
+    <Navbar bg="black"  expand="lg">
+      <Container >
+        <Navbar.Brand className='text-light' href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link className='text-light' href="#home">Home</Nav.Link>
+            <Nav.Link className='text-light' href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
 export default Header;
